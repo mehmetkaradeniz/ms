45602401c1324a13f95a42c8b3e54a01f6426752
Mehmet Karadeniz <mehmetkz61@gmail.com>
2021-10-29 22:54:29 +0300

    - msdiscountapi (aspnetcore web api) - msdiscountdbui (pgadmin) - msdiscountdb (postgres)

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,51 @@
+﻿using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Discount.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository _repository;
+
+        public DiscountController(IDiscountRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet("{productName}", Name = "Get")]
+        [ProducesResponseType(typeof(CouponEntity), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<CouponEntity>> Get(string productName)
+        {
+            var coupon = await _repository.Get(productName);
+            return Ok(coupon);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(CouponEntity), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<CouponEntity>> Create([FromBody] CouponEntity coupon)
+        {
+            await _repository.Create(coupon);
+            return CreatedAtRoute("Get", new { productName = coupon.ProductName }, coupon);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(CouponEntity), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<CouponEntity>> Update([FromBody] CouponEntity coupon)
+        {
+            return Ok(await _repository.Update(coupon));
+        }
+
+        [HttpDelete("{productName}", Name = "Delete")]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> Delete(string productName)
+        {
+            return Ok(await _repository.Delete(productName));
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,16 @@
+﻿using Discount.API.Entities;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<CouponEntity> Get(string productName);
+
+        Task<bool> Create(CouponEntity coupon);
+
+        Task<bool> Update(CouponEntity coupon);
+
+        Task<bool> Delete(string productName);
+    }
+}
